288e558e10e3012ee67661c05c33cb5e27ad626b
Will Forsyth <will.forsyth@drummondcentral.co.uk>
2016-06-14 15:35:51 +0100

    come tweaks

--- dev/_assets/scripts/components/countdown.js
+++ dev/_assets/scripts/components/countdown.js
@@ -1,22 +1,22 @@
 /*global define:false */
 $.fn.countdown= function (options) {
-  console.clear();
-    var words = require('word-list-json');
-    console.log(words.key);
-    var letters = ['k','l','f','h','t','r','s','d','a'];
-    var wordsLength = words.length;
-    var sortedLetters = letters.sort();
-
-    for (i = 0; i < wordsLength; i++) {
-      var results = [];
-      // split words into and sort alphabetically
-      var sortedWords = words[i].toString().split('').sort();
-
-      var matchingLetters = _.intersection(sortedWords, sortedLetters);
-
-      if(matchingLetters.length === sortedWords.length){
-        console.log(matchingLetters);
-      }
-
+    var wordsJson = require('word-list-json');
+    var letters = ['b','e','d','n','e','r','s','h'];
+    // for loop to get keys and values
+    for(key in wordsJson) {
+        // save the values to prop
+        var prop = wordsJson[key];
+        // Get the length of the values
+        var wordsLength = prop.length;
+        // using uderscore find all the words that contain the letters
+        var matchingLetters = _.intersection(prop, letters);
+        // if the length of the of the word with the matching items is the same
+        // length as a word in the dictionay it must be a full word
+        if(matchingLetters.length === prop.length){
+          // find the key of the macthing words
+          var result = matchingLetters && key;
+          // log it out
+          console.log(wordsJson[result]);
+        }
     }
 };
